bf0aa1fa6e445e3266b9ff970a5ca3bc169b867d
lauvsong <hyehye9355@naver.com>
2021-07-16 02:15:03 -0400

    Confirm fread output

--- main.cpp
+++ main.cpp
@@ -10,7 +10,11 @@ uint32_t make_nbo(char* filename){
     }
 
     uint32_t res;
-    fread(&res, 1, sizeof(uint32_t), fd);
+    size_t readlen = fread(&res, 1, sizeof(uint32_t), fd);
+    if (readlen != sizeof(uint32_t)){
+        fprintf(stderr, "fread return %lu\n", readlen);
+        return -1;
+    }
     fclose(fd);
 
     return ntohl(res);
@@ -19,14 +23,14 @@ uint32_t make_nbo(char* filename){
 int main(int argc, char* argv[]){
 
     if (argc != 3){
-        printf("Usage: %s <file1.bin> <file2.bin>", argv[0]);
+        printf("Usage: %s <file1>.bin <file2>.bin", argv[0]);
         return -1;
     }
 
     uint32_t num1 = make_nbo(argv[1]);
     uint32_t num2 = make_nbo(argv[2]);
-    uint32_t res = num1+num2;
 
+    uint32_t res = num1+num2;
     printf("%d(%#x) + %d(%#x) = %d(%#x)\n", num1, num1, num2, num2, res, res);
 
     return 0;
